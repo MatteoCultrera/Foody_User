6e9b2a867ca017571c169bf97d8ccf88210c086e
MatteoCultrera <mcultrera94@icloud.com>
2019-03-20 21:43:04 +0100

    Full Project

--- app/src/main/java/com/example/foodyuser/Setup.java
+++ app/src/main/java/com/example/foodyuser/Setup.java
@@ -2,24 +2,12 @@ package com.example.foodyuser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class Setup extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        //to hide the title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
-        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-        //        WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
         setContentView(R.layout.activity_setup);
-
-
     }
 }
