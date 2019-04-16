637feca71b151452dac9f131bf1e3a5e1c434d8d
Daniele <danieleleto94@gmail.com>
2019-04-16 22:35:27 +0200

    Many bug fix plus reservations Json implemented

--- foody_restaurant/src/main/java/com/example/foodyrestaurant/ReservationFragment.java
+++ foody_restaurant/src/main/java/com/example/foodyrestaurant/ReservationFragment.java
@@ -1,6 +1,7 @@
 package com.example.foodyrestaurant;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,7 +12,9 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ReservationFragment extends Fragment {
 
@@ -37,39 +40,101 @@ public class ReservationFragment extends Fragment {
     }
 
     private void init(View view){
+        String json;
+        final File storageDir = Objects.requireNonNull(getActivity()).getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        final String JSON_PATH = "reservations.json";
+        File file = new File(storageDir, JSON_PATH);
+        ArrayList<Reservation> reservations;
+
         LinearLayoutManager llm = new LinearLayoutManager(view.getContext());
         reservation.setLayoutManager(llm);
 
-        ArrayList<Reservation> reservations = new ArrayList<>();
-        ArrayList<Dish> dishes = new ArrayList<>();
-        Dish toAdd = new Dish("Margherita","pizza",2.0f, null);
-        toAdd.setQuantity(1);
-        dishes.add(toAdd);
-        toAdd = new Dish("Patatine","pizza",2.0f, null);
-        toAdd.setQuantity(1);
-        dishes.add(toAdd);
-        toAdd = new Dish("Coca Cola","pizza",2.0f, null);
-        toAdd.setQuantity(1);
-        dishes.add(toAdd);
-        Reservation res1 = new Reservation(getResources().getString(R.string.reservation) + "252850", dishes, Reservation.prepStatus.PENDING,"12:30",
-                "Daniele Leto", "3469489722", "Suonare campanello giallo", "Foody Expert");
-        Reservation res2 = new Reservation(getResources().getString(R.string.reservation) + "252851", dishes, Reservation.prepStatus.PENDING,"12:45",
-                "Matteo Cultrera","3333333333", null, "Foody Beginner");
-        Reservation res3 = new Reservation(getResources().getString(R.string.reservation) + "300000", dishes, Reservation.prepStatus.PENDING, "12:45",
-                "Matteo Cultrera","3333333333", null, "Foody Beginner");
-        Reservation res4 = new Reservation(getResources().getString(R.string.reservation) + "400000", dishes, Reservation.prepStatus.PENDING,"12:30",
-                "Daniele Leto", "3469489722", "Suonare campanello giallo", "Foody Expert");
-        Reservation res5 = new Reservation(getResources().getString(R.string.reservation) + "500000", dishes, Reservation.prepStatus.PENDING,"12:45",
-                "Fabio Carfì", "3469489722", "Suonare campanello giallo, perchè ho bisogno " +
-                "della pizza calda e consegnata in tempo", "Foody Expert");
-        Reservation res6 = new Reservation(getResources().getString(R.string.reservation) + "600000", dishes, Reservation.prepStatus.PENDING, "12:45",
-                "Mattia Cara", "3469489722", "Suonare campanello giallo", "Foody Expert");
-        reservations.add(res1);
-        reservations.add(res2);
-        reservations.add(res3);
-        reservations.add(res4);
-        reservations.add(res5);
-        reservations.add(res6);
+        JsonHandler jsonHandler = new JsonHandler();
+
+        reservations = jsonHandler.getReservations(file);
+
+        if(reservations.size() == 0) {
+            reservations = new ArrayList<>();
+
+            ArrayList<Dish> dishes = new ArrayList<>();
+            Dish toAdd = new Dish("Margherita", "pizza", 2.0f, null);
+            toAdd.setQuantity(1);
+            dishes.add(toAdd);
+            toAdd = new Dish("Patatine", "pizza", 2.0f, null);
+            toAdd.setQuantity(3);
+            dishes.add(toAdd);
+            toAdd = new Dish("Coca Cola", "pizza", 2.0f, null);
+            toAdd.setQuantity(6);
+            dishes.add(toAdd);
+            Reservation res1 = new Reservation(getResources().getString(R.string.reservation) + "252850",
+                    dishes, Reservation.prepStatus.PENDING, false,"12:30", "Daniele Leto",
+                    "3469489722", "Suonare campanello giallo", "Foody Expert",
+                    "danieleleto@gmail.com", "Viale Dei Nanni 5, Torino");
+            dishes = new ArrayList<>();
+            toAdd = new Dish("Cotoletta", "pizza", 2.0f, null);
+            toAdd.setQuantity(2);
+            dishes.add(toAdd);
+            toAdd = new Dish("Quattro Stagioni", "pizza", 2.0f, null);
+            toAdd.setQuantity(4);
+            dishes.add(toAdd);
+            Reservation res2 = new Reservation(getResources().getString(R.string.reservation) + "252851",
+                    dishes, Reservation.prepStatus.PENDING, false,"13:00", "Matteo Cultrera",
+                    "3333333333", null, "Foody Beginner",
+                    "matteocult@gmail.com", "Via Abruzzi 37, Torino");
+            dishes = new ArrayList<>();
+            toAdd = new Dish("Coca Cola", "pizza", 2.0f, null);
+            toAdd.setQuantity(3);
+            dishes.add(toAdd);
+            toAdd = new Dish("Vegetariana", "pizza", 2.0f, null);
+            toAdd.setQuantity(4);
+            dishes.add(toAdd);
+            Reservation res3 = new Reservation(getResources().getString(R.string.reservation) + "300000",
+                    dishes, Reservation.prepStatus.PENDING, false,"13:00", "Matteo Cultrera",
+                    "3333333333", null, "Foody Beginner",
+                    "matteocult@gmail.com", "Via Abruzzi 37, Torino");
+            dishes = new ArrayList<>();
+            toAdd = new Dish("Margherita", "pizza", 2.0f, null);
+            toAdd.setQuantity(1);
+            dishes.add(toAdd);
+            toAdd = new Dish("Patatine", "pizza", 2.0f, null);
+            toAdd.setQuantity(3);
+            dishes.add(toAdd);
+            toAdd = new Dish("Coca Cola", "pizza", 2.0f, null);
+            toAdd.setQuantity(6);
+            dishes.add(toAdd);
+            Reservation res4 = new Reservation(getResources().getString(R.string.reservation) + "400000",
+                    dishes, Reservation.prepStatus.PENDING, false,"12:30", "Mattia Cara",
+                    "3469489722", "Suonare campanello giallo", "Foody Expert",
+                    "mattCara@gmail.com", "Via Boggio 55, Torino");
+            dishes = new ArrayList<>();
+            toAdd = new Dish("Cotoletta", "pizza", 2.0f, null);
+            toAdd.setQuantity(2);
+            dishes.add(toAdd);
+            toAdd = new Dish("Quattro Stagioni", "pizza", 2.0f, null);
+            toAdd.setQuantity(4);
+            toAdd = new Dish("Margherita", "pizza", 2.0f, null);
+            toAdd.setQuantity(1);
+            dishes.add(toAdd);
+            toAdd = new Dish("Patatine", "pizza", 2.0f, null);
+            toAdd.setQuantity(3);
+            dishes.add(toAdd);
+            toAdd = new Dish("Coca Cola", "pizza", 2.0f, null);
+            toAdd.setQuantity(6);
+            dishes.add(toAdd);
+            Reservation res5 = new Reservation(getResources().getString(R.string.reservation) + "500000",
+                    dishes, Reservation.prepStatus.PENDING, false,"12:45", "Fabio Carfì",
+                    "3469489722", "Suonare campanello giallo, perchè ho bisogno della pizza calda e consegnata in tempo",
+                    "Foody Beginner", "fabCarfi@gmail.com", "Via Tripoli 101, Torino");
+            reservations.add(res1);
+            reservations.add(res2);
+            reservations.add(res3);
+            reservations.add(res4);
+            reservations.add(res5);
+        }
+
+        json = jsonHandler.resToJSON(reservations);
+        jsonHandler.saveStringToFile(json, file);
+
         RVAdapterRes adapter = new RVAdapterRes(reservations);
 
         reservation.setAdapter(adapter);
