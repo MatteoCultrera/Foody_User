bf060465f90c5dbd50d097ec08f9ec9dcc6b377f
Fabio <fabio.carfi95@gmail.com>
2019-04-27 13:29:17 +0200

    Added the fragment in Fooddy_biker

--- foody_biker/src/main/java/com/example/foodybiker/MainActivity.java
+++ foody_biker/src/main/java/com/example/foodybiker/MainActivity.java
@@ -1,15 +1,117 @@
 package com.example.foodybiker;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private enum TabState{
+        MAP,
+        ORDERS,
+        USER,
+    }
+
+    private Fragment map = new MapFragment();
+    private Fragment reservations = new ReservationFragment();
+    private Fragment user = new UserFragment();
+    private final FragmentManager fm = getSupportFragmentManager();
+    private Fragment active;
+    TabState stateApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, User.class));
-        finish();
+        setContentView(R.layout.bottom_bar);
+        if (savedInstanceState != null) {
+            String lastFragment = savedInstanceState.getString("lastFragment", null);
+            if (lastFragment != null) {
+                if (lastFragment.compareTo("discover") == 0) {
+                    active = map;
+                } else if (lastFragment.compareTo("reservations") == 0) {
+                    active = reservations;
+                } else if (lastFragment.compareTo("user") == 0) {
+                    active = user;
+                }
+            }
+            init();
+            return;
+        }
+        map = new MapFragment();
+        reservations = new ReservationFragment();
+        user = new UserFragment();
+        fm.beginTransaction().add(R.id.mainFrame, user, "3").show(user).commit();
+        fm.beginTransaction().add(R.id.mainFrame, reservations, "2").commit();
+        fm.beginTransaction().add(R.id.mainFrame, map, "1").commit();
+        active = user;
+        init();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        if (active == map){
+            outState.putString("lastFragment", "discover");
+        } else if (active == reservations){
+            outState.putString("lastFragment", "reservations");
+        } else if (active == user){
+            outState.putString("lastFragment", "user");
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        try {
+            fm.beginTransaction().detach(active).attach(active).commit();
+        } catch (IllegalStateException e){
+            e.getMessage();
+        }
+    }
+
+    private void init(){
+        BottomNavigationView bottomBar = findViewById(R.id.bottom_navigation);
+        bottomBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if(id == R.id.discover && active != map){
+                    FragmentTransaction transaction = fm.beginTransaction();
+                    transaction.setCustomAnimations(R.anim.enter_from_left,R.anim.exit_to_right);
+                    transaction.replace(R.id.mainFrame, map).commit();
+                    active = map;
+                    return true;
+                }else if(id == R.id.orders && active != reservations){
+                    if(active == map){
+                        FragmentTransaction transaction =fm.beginTransaction();
+                        transaction.setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_left);
+                        transaction.replace(R.id.mainFrame, reservations).commit();
+                    }else if(active == user) {
+                        FragmentTransaction transaction = fm.beginTransaction();
+                        transaction.setCustomAnimations(R.anim.enter_from_left, R.anim.exit_to_right);
+                        transaction.replace(R.id.mainFrame, reservations).commit();
+                    }
+                    active = reservations;
+                    return true;
+                }else if(id == R.id.prof && active != user){
+                    FragmentTransaction transaction = fm.beginTransaction();
+                    transaction.setCustomAnimations(R.anim.enter_from_right,R.anim.exit_to_left);
+                    transaction.replace(R.id.mainFrame, user).commit();
+                    active = user;
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 }
