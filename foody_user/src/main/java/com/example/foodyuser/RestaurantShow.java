861799a85133a7b47315f51668aef3cf3d61cc92
Mattia <mattia.cara@outlook.it>
2019-05-06 18:02:35 +0200

    query on restaurant show

--- foody_user/src/main/java/com/example/foodyuser/RestaurantShow.java
+++ foody_user/src/main/java/com/example/foodyuser/RestaurantShow.java
@@ -27,6 +27,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -197,8 +198,8 @@ public class RestaurantShow extends AppCompatActivity {
             return;
 
         DatabaseReference database = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference ref = database.child("restaurantsInfo").child(reName);
-        ref.addValueEventListener(new ValueEventListener() {
+        Query query = database.child("restaurantsInfo").child(reName);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for (DataSnapshot ds : dataSnapshot.getChildren()) {
