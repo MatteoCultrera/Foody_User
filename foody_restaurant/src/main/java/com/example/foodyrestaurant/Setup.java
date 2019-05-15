1ada5f2455f9efe474916e9319e31617315307a6
Mattia <mattia.cara@outlook.it>
2019-05-15 18:06:56 +0200

    fixes on restaurant

--- foody_restaurant/src/main/java/com/example/foodyrestaurant/Setup.java
+++ foody_restaurant/src/main/java/com/example/foodyrestaurant/Setup.java
@@ -98,9 +98,9 @@ public class Setup extends AppCompatActivity {
         setContentView(R.layout.activity_setup);
         firebaseAuth = FirebaseAuth.getInstance();
         storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-
+        Bundle extras = getIntent().getExtras();
+        pathImage = extras.getString("imagePath", null);
         init();
-
         editImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -112,7 +112,6 @@ public class Setup extends AppCompatActivity {
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-
         outState.putString("name", name.getText().toString());
         outState.putString("email", email.getText().toString());
         outState.putString("address", address.getText().toString());
@@ -166,21 +165,6 @@ public class Setup extends AppCompatActivity {
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-
-        File f = new File(storageDir, PLACEHOLDER_CAMERA);
-
-
-        RequestOptions glideOptions = new RequestOptions()
-                .signature(new ObjectKey(f.getPath()+f.lastModified()));
-
-        if(f.exists())
-
-            Glide
-                    .with(this)
-            .load(f)
-            .apply(glideOptions)
-            .into(profilePicture);
-
         name.setText(savedInstanceState.getString("name", ""));
         email.setText(savedInstanceState.getString("email", ""));
         address.setText(savedInstanceState.getString("address", ""));
@@ -435,13 +419,9 @@ public class Setup extends AppCompatActivity {
         errorPhone.setText("");
         errorAddress.setText("");
 
-        String imagePath = sharedPref.getString("Path", "");
-
-
-
-        if(imagePath.length()>0){
+        if(pathImage != null) {
             StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
-            mStorageRef.child(imagePath).getDownloadUrl()
+            mStorageRef.child(pathImage).getDownloadUrl()
                     .addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
                         public void onSuccess(Uri uri) {
@@ -449,19 +429,23 @@ public class Setup extends AppCompatActivity {
                                     .with(profilePicture.getContext())
                                     .load(uri)
                                     .into(profilePicture);
-                            //TODO: salvare l'uri qua nel path dell'immagine profilo
                         }
-                    }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Glide
-                            .with(profilePicture.getContext())
-                            .load(R.drawable.profile_placeholder)
-                            .into(profilePicture);
-                }
-            });
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Glide
+                                    .with(profilePicture.getContext())
+                                    .load(R.drawable.profile_placeholder)
+                                    .into(profilePicture);
+                        }
+                    });
+        }else{
+            Glide
+                    .with(profilePicture.getContext())
+                    .load(R.drawable.profile_placeholder)
+                    .into(profilePicture);
         }
-
 
         name.setText(sharedPref.getString("name", getResources().getString(R.string.name_hint)));
         email.setText(sharedPref.getString("email", getResources().getString(R.string.email_hint)));
--- foody_restaurant/src/main/java/com/example/foodyrestaurant/UserFragment.java
+++ foody_restaurant/src/main/java/com/example/foodyrestaurant/UserFragment.java
@@ -57,6 +57,7 @@ public class UserFragment extends Fragment {
     private SharedPreferences.Editor edit;
     private MaterialButton logout;
     private String imagePath;
+    private RestaurantInfo info;
 
     public UserFragment() {}
 
@@ -70,32 +71,7 @@ public class UserFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
     }
 
-    private void saveBitmap(Bitmap bitmap,String path){
-        if(bitmap!=null){
-            try {
-                FileOutputStream outputStream = null;
-                try {
-                    outputStream = new FileOutputStream(path); //here is set your file path where you want to save or also here you can set file object directly
-
-                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream); // bitmap is your Bitmap instance, if you want to compress it you can compress reduce percentage
-                } catch (Exception e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (outputStream != null) {
-                            outputStream.close();
-                        }
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    private void init(View view){
+    private void init(View view) {
         sharedPref = view.getContext().getSharedPreferences("myPreference", MODE_PRIVATE);
         edit = sharedPref.edit();
         firebaseAuth = FirebaseAuth.getInstance();
@@ -119,32 +95,33 @@ public class UserFragment extends Fragment {
         foodType = view.findViewById(R.id.food_type);
 
         //setup of the Shared Preferences to save value in (key, value) format
-        if (!email.getText().toString().equals("email")) {
-            final DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("restaurantsInfo");
-            Query query = database.child(firebaseAuth.getCurrentUser().getUid());
-            query.addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                        RestaurantInfo info = ds.getValue(RestaurantInfo.class);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("restaurantsInfo");
+                Query query = database.child(firebaseAuth.getCurrentUser().getUid()).child("info");
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        info = dataSnapshot.getValue(RestaurantInfo.class);
                         name.setText(info.getUsername());
                         email.setText(info.getEmail());
                         address.setText(info.getAddress());
                         phoneNumber.setText(info.getNumberPhone());
                         int deliveryPrice;
                         deliveryPrice = info.getDeliveryPrice();
-                        double price = 0.5 *  deliveryPrice;
-                        String text = String.format(Locale.UK,"%.2f", price) + getResources().getString(R.string.value);
+                        double price = 0.5 * deliveryPrice;
+                        String text = String.format(Locale.UK, "%.2f", price) + getResources().getString(R.string.value);
                         delivPrice.setText(text);
                         String[] foodCategories = getResources().getStringArray(R.array.foodcategory_array);
                         ArrayList<Integer> cuisine = info.getCuisineTypes();
                         imagePath = info.getImagePath();
                         String cuisineText = "";
-                        if (cuisine != null){
-                            for(int i = 0; i < cuisine.size(); i++){
+                        if (cuisine != null) {
+                            for (int i = 0; i < cuisine.size(); i++) {
                                 cuisineText += foodCategories[cuisine.get(i)];
-                                if(i!= cuisine.size()-1)
-                                    cuisineText+=", ";
+                                if (i != cuisine.size() - 1)
+                                    cuisineText += ", ";
                             }
                         }
                         foodType.setText(cuisineText);
@@ -164,57 +141,67 @@ public class UserFragment extends Fragment {
                             edit.putString("address", info.getAddress());
                         if (!phoneNumber.getText().toString().equals(getResources().getString(R.string.phone_hint)))
                             edit.putString("phoneNumber", info.getNumberPhone());
-                        if (!info.getDaysTime().get(0).equals(getResources().getString(R.string.Closed))){
+                        if (!info.getDaysTime().get(0).equals(getResources().getString(R.string.Closed))) {
                             edit.putBoolean("monState", true);
                         }
-                        if (!info.getDaysTime().get(1).equals(getResources().getString(R.string.Closed))){
+                        if (!info.getDaysTime().get(1).equals(getResources().getString(R.string.Closed))) {
                             edit.putBoolean("tueState", true);
                         }
-                        if (!info.getDaysTime().get(2).equals(getResources().getString(R.string.Closed))){
+                        if (!info.getDaysTime().get(2).equals(getResources().getString(R.string.Closed))) {
                             edit.putBoolean("wedState", true);
                         }
-                        if (!info.getDaysTime().get(3).equals(getResources().getString(R.string.Closed))){
+                        if (!info.getDaysTime().get(3).equals(getResources().getString(R.string.Closed))) {
                             edit.putBoolean("thuState", true);
                         }
-                        if (!info.getDaysTime().get(4).equals(getResources().getString(R.string.Closed))){
+                        if (!info.getDaysTime().get(4).equals(getResources().getString(R.string.Closed))) {
                             edit.putBoolean("friState", true);
                         }
-                        if (!info.getDaysTime().get(5).equals(getResources().getString(R.string.Closed))){
+                        if (!info.getDaysTime().get(5).equals(getResources().getString(R.string.Closed))) {
                             edit.putBoolean("satState", true);
                         }
-                        if (!info.getDaysTime().get(6).equals(getResources().getString(R.string.Closed))){
+                        if (!info.getDaysTime().get(6).equals(getResources().getString(R.string.Closed))) {
                             edit.putBoolean("sunState", true);
                         }
 
                         StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
-
-                        if(imagePath!=null){
+                        if (imagePath != null) {
                             mStorageRef.child(imagePath).getDownloadUrl()
-                                    .addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                        @Override
-                                        public void onSuccess(Uri uri) {
-                                            Glide
-                                                    .with(profilePicture.getContext())
-                                                    .load(uri)
-                                                    .into(profilePicture);
-                                        }
-                                    }).addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-
-                                    Glide
-                                            .with(profilePicture.getContext())
-                                            .load(R.drawable.profile_placeholder)
-                                            .into(profilePicture);
-                                }
+                                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                    @Override
+                                    public void onSuccess(Uri uri) {
+                                        Glide
+                                                .with(profilePicture.getContext())
+                                                .load(uri)
+                                                .into(profilePicture);
+                                        Glide
+                                                .with(profileShadow.getContext())
+                                                .load(R.drawable.shadow)
+                                                .into(profileShadow);
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Glide
+                                                .with(profilePicture.getContext())
+                                                .load(R.drawable.profile_placeholder)
+                                                .into(profilePicture);
+                                        Glide
+                                                .with(profileShadow.getContext())
+                                                .load(R.drawable.shadow)
+                                                .into(profileShadow);
+                                    }
                             });
-                        }else{
+                        } else {
                             Glide
                                     .with(profilePicture.getContext())
                                     .load(R.drawable.profile_placeholder)
                                     .into(profilePicture);
+                            Glide
+                                    .with(profileShadow.getContext())
+                                    .load(R.drawable.shadow)
+                                    .into(profileShadow);
                         }
-
                         edit.putString("monTime", info.getDaysTime().get(0));
                         edit.putString("tueTime", info.getDaysTime().get(1));
                         edit.putString("wedTime", info.getDaysTime().get(2));
@@ -225,75 +212,35 @@ public class UserFragment extends Fragment {
                         edit.putString("Path", imagePath);
                         edit.apply();
                     }
-                }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Log.d("SWSW", databaseError.getMessage());
-                    name.setText(sharedPref.getString("name", getResources().getString(R.string.name_hint)));
-                    email.setText(sharedPref.getString("email", getResources().getString(R.string.email_hint)));
-                    address.setText(sharedPref.getString("address", getResources().getString(R.string.address_hint)));
-                    phoneNumber.setText(sharedPref.getString("phoneNumber", getResources().getString(R.string.phone_hint)));
-                    delivPrice.setText(sharedPref.getInt("delivPrice", 5));
-                    foodType.setText(sharedPref.getString("foodType", ""));
-                    monTime.setText(sharedPref.getString("monTime", getResources().getString(R.string.Closed)));
-                    tueTime.setText(sharedPref.getString("tueTime", getResources().getString(R.string.Closed)));
-                    wedTime.setText(sharedPref.getString("wedTime", getResources().getString(R.string.Closed)));
-                    thuTime.setText(sharedPref.getString("thuTime", getResources().getString(R.string.Closed)));
-                    friTime.setText(sharedPref.getString("friTime", getResources().getString(R.string.Closed)));
-                    satTime.setText(sharedPref.getString("satTime", getResources().getString(R.string.Closed)));
-                    sunTime.setText(sharedPref.getString("sunTime", getResources().getString(R.string.Closed)));
-                }
-            });
-        }
-
-        File f = new File(storageDir, PROFILE_IMAGE);
-
-        RequestOptions glideOptions = new RequestOptions()
-                .signature(new ObjectKey(f.getPath()+f.lastModified()));
-
-        if(f.exists()){
-            Glide
-                    .with(this)
-                    .load(f)
-                    .apply(glideOptions)
-                    .into(profilePicture);
-        } else {
-            StorageReference mStorageRef = FirebaseStorage.getInstance().getReference();
-            mStorageRef.child("images/restaurants/" + firebaseAuth.getCurrentUser().getUid() + "_profile.jpeg").getDownloadUrl()
-                    .addOnSuccessListener(new OnSuccessListener<Uri>() {
-                        @Override
-                        public void onSuccess(Uri uri) {
-                            Glide
-                                    .with(profilePicture.getContext())
-                                    .load(uri)
-                                    .into(profilePicture);
-
-                            Glide
-                                    .with(profileShadow.getContext())
-                                    .load(R.drawable.shadow)
-                                    .into(profileShadow);
-                            //TODO: salvare l'uri qua nel path dell'immagine profilo
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Glide
-                            .with(profilePicture.getContext())
-                            .load(R.drawable.profile_placeholder)
-                            .into(profilePicture);
-                }
-            });
-        }
-
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        name.setText(sharedPref.getString("name", getResources().getString(R.string.name_hint)));
+                        email.setText(sharedPref.getString("email", getResources().getString(R.string.email_hint)));
+                        address.setText(sharedPref.getString("address", getResources().getString(R.string.address_hint)));
+                        phoneNumber.setText(sharedPref.getString("phoneNumber", getResources().getString(R.string.phone_hint)));
+                        delivPrice.setText(sharedPref.getInt("delivPrice", 5));
+                        foodType.setText(sharedPref.getString("foodType", ""));
+                        monTime.setText(sharedPref.getString("monTime", getResources().getString(R.string.Closed)));
+                        tueTime.setText(sharedPref.getString("tueTime", getResources().getString(R.string.Closed)));
+                        wedTime.setText(sharedPref.getString("wedTime", getResources().getString(R.string.Closed)));
+                        thuTime.setText(sharedPref.getString("thuTime", getResources().getString(R.string.Closed)));
+                        friTime.setText(sharedPref.getString("friTime", getResources().getString(R.string.Closed)));
+                        satTime.setText(sharedPref.getString("satTime", getResources().getString(R.string.Closed)));
+                        sunTime.setText(sharedPref.getString("sunTime", getResources().getString(R.string.Closed)));
+                    }
+                });
+            }
+        }).start();
         editMode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), Setup.class);
                 File pl = new File(storageDir, PLACEHOLDER_CAMERA);
-                if(!pl.delete()){
+                if (!pl.delete()) {
                     System.out.println("Delete Failure");
                 }
+                intent.putExtra("imagePath", info.getImagePath());
                 startActivity(intent);
             }
         });
