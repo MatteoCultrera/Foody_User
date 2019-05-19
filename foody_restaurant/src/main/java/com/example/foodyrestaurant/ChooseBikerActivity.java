022793b5e0c2a9faacb6db5a1b3f990a62fd8485
MatteoCultrera <mcultrera94@icloud.com>
2019-05-19 09:36:52 +0200

    Merge branch 'master' of https://github.com/MatteoCultrera/Foody_User into matteoBranch
    # Conflicts:
    #	foody_restaurant/src/main/AndroidManifest.xml

--- foody_restaurant/src/main/java/com/example/foodyrestaurant/ChooseBikerActivity.java
+++ foody_restaurant/src/main/java/com/example/foodyrestaurant/ChooseBikerActivity.java
@@ -3,11 +3,33 @@ package com.example.foodyrestaurant;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import java.util.ArrayList;
+
 public class ChooseBikerActivity extends AppCompatActivity {
+
+    private ArrayList<BikerInfo> bikers;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_biker);
+    }
+
+    private void init(){
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        final DatabaseReference database = FirebaseDatabase.getInstance().getReference().child("Bikers");
+        Query query = database.child(firebaseUser.getUid());
+
+
+
     }
 }
