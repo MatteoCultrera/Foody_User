c7d2792706e2e158190f20f09bac6acc97642977
Fabio <fabio.carfi95@gmail.com>
2019-06-07 16:39:26 +0200

    changed imagePathRest for the DB

--- foody_user/src/main/java/com/example/foodyuser/ShowUserReviewActivity.java
+++ foody_user/src/main/java/com/example/foodyuser/ShowUserReviewActivity.java
@@ -3,6 +3,7 @@ package com.example.foodyuser;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +15,12 @@ import com.example.foody_library.Review;
 import com.github.ybq.android.spinkit.SpinKitView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -88,7 +95,19 @@ public class ShowUserReviewActivity extends AppCompatActivity {
     }
 
     private void fetchReviews(final boolean hasImages){
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        Query query = database.child("userReviews");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     private void fetchRestaurantImages(final String imagePath){
